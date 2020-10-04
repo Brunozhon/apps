910d8e7191e6e454b6556c5517f84a34f57708e8
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2020-10-04 14:49:26 -0400

    Initial commit

---
+++ assets/code/js/bruno.js
@@ -0,0 +1,67 @@
+// BrunoJS
+var brunojs = {
+  createClass: function(name, constr, other) {
+    return eval("class " + name + " { constructor() {" + constr + "} " + other + "}");
+  },
+  prototype: function(_class, key, value) {
+    eval(_class + ".prototype." + key + " = " + value);
+  }
+};
+// BrunoJS DOM
+var brunojsdom = {
+  render: function(_class, exec) {
+    exec.innerHTML = _class.render();
+  },
+  select: function(value) {
+    return document.querySelectorAll(value);
+  }
+};
+// Prototype
+Date.prototype.toReadableDate = function() {
+  var txt;
+  switch(this.getMonth()) {
+    case 0:
+      txt += "January";
+      break;
+    case 1:
+      txt += "Feburary";
+      break;
+    case 2:
+      txt += "March";
+      break;
+    case 3:
+      txt += "April";
+      break;
+    case 4:
+      text += "May";
+      break;
+    case 5:
+      text += "June";
+      break;
+    case 6:
+      txt += "July";
+      break;
+    case 7:
+      txt += "August";
+      break;
+    case 8:
+      txt += "September";
+      break;
+    case 9:
+      txt += "October";
+      break;
+    case 10:
+      txt += "Novenber";
+      break;
+    case 11:
+      txt += "December";
+      break;
+    default:
+      txt += "Unknown";
+      break;
+  }
+  txt += " " + this.getDay().toString() ", " + this.getFullYear().toString() ", " +
+  this.getHours().toString() + ":" + this.getMinutes().toString() + ":" + this.getSeconds().toString() + 
+  "." + this.getMilliseconds();
+  return txt;
+}
